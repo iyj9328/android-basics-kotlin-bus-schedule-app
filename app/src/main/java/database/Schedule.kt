3a49f334ce3f94a3cd9014c5b419952c114bd9e5
필립_이원준 <iyj93287@gmail.com>
2022-01-05 01:25:20 +0900

    :package: Database패키지 추가 및 버스 정류장 도착시간 Entity추가

---
+++ app/src/main/java/database/Schedule.kt
@@ -0,0 +1,18 @@
+package database
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Schedule(
+    @PrimaryKey
+    private val id : Int,
+
+    @NonNull @ColumnInfo(name = "station_name")
+    private val stationName : String,
+
+    @NonNull @ColumnInfo(name = "arrival_time")
+    private val arrivalTime : String
+)
