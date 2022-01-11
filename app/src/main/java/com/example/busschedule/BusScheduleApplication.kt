3e0550f0c327c1ba839a9b408d192bec6df5a691
필립_이원준 <iyj9328@afreecatv.com>
2022-01-11 17:51:26 +0900

    RoomDatabse 초기화 코드 삽입

--- app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++ app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -1,8 +1,10 @@
 package com.example.busschedule
 
 import android.app.Application
+import com.example.busschedule.database.AppDatabase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class BusScheduleApplication : Application() {
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
 }
